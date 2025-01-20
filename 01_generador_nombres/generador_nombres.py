d8812128a165c18cde5caca58e59fc5d4c5aa191
AbbyEnRedes <abbyenredes@gmail.com>
2025-01-20 11:41:13 +0100

    add new file

---
+++ 01_generador_nombres/generador_nombres.py
@@ -0,0 +1,4 @@
+print("Bienvenido, te voy a ayudar a crear un nombre unico para tu marca")
+animal = input("Dime un animal que te inspire: ")
+adjetivo = input("Dime un adjetivo que describa tu momento favorito: ")
+print("¡Enhorabuena!\n El nombre de tu negocio es: " "\"",animal, adjetivo,"\"")
